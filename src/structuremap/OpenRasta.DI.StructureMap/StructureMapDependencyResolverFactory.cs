7ebabc606d6cc858228f647ec621513b86400d2f
Jamie Coates <jamie.coates@linn.co.uk>
2011-06-09 15:00:22 +0100

    converted structuremap solution to vs2010. added structuremapdependencyresolverfactory to provide factory access to the dependencyresolver

---
+++ src/structuremap/OpenRasta.DI.StructureMap/StructureMapDependencyResolverFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRasta.DI.StructureMap
+{
+    public class StructureMapDependencyResolverFactory : IDependencyResolverAccessor
+    {
+        public IDependencyResolver Resolver
+        {
+            get { return new StructureMapDependencyResolver(); }
+        }
+    }
+}
